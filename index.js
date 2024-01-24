4a2ea62f9b4f2f643f64210fcdfa0fb9a5c0c1e8
Kaio CamposViana <KCampo246@ignitiongroup.co.za>
2024-01-24 09:34:33 +0000

    update

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const path = require('path');  // Certifique-se de incluir o módulo path
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-// Define o diretório para servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
